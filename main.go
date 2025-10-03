b8045607406a4895a76619a37780dfbc5902a64f
0xHansLee <hansol.lee@storyprotocol.xyz>
2025-10-03 15:42:39 +0900

    test report data of quote

--- main.go
+++ main.go
@@ -3,19 +3,17 @@ package main
 import (
 	"encoding/hex"
 	"fmt"
-	egoenclave "github.com/edgelesssys/ego/enclave"
-	"github.com/ethereum/go-ethereum/common/hexutil"
-	"github.com/hanslee/go-sgx-dcap-poc/enclave"
+	"github.com/edgelesssys/ego/enclave"
 	"log"
 )
 
 func main() {
 	// 1. Load or generate QE key pair
-	pub, _, err := enclave.LoadOrCreateSealedQEKey()
-	if err != nil {
-		log.Fatalf("failed to generate keypair: %v", err)
-	}
-	fmt.Println("Public key: ", hexutil.Encode(pub))
+	//pub, _, err := enclave.LoadOrCreateSealedQEKey()
+	//if err != nil {
+	//	log.Fatalf("failed to generate keypair: %v", err)
+	//}
+	//fmt.Println("Public key: ", hexutil.Encode(pub))
 
 	// 2. Create a new Eth Client
 	//ethClient, err := eth.NewEthClient()
@@ -24,11 +22,11 @@ func main() {
 	//}
 
 	// 3. Get real quote from enclave
-	fmt.Println("pub key as a report data", hexutil.Encode(pub))
-	report, err := egoenclave.GetRemoteReport(pub)
-	if err != nil {
-		log.Fatalf("failed to get quote: %v", err)
-	}
+	//fmt.Println("pub key as a report data", hexutil.Encode(pub))
+	//report, err := egoenclave.GetRemoteReport(pub)
+	//if err != nil {
+	//	log.Fatalf("failed to get quote: %v", err)
+	//}
 
 	// 4. Extract FMSPC from quote
 	//fmspc := enclave.ExtractFmspc(report)
@@ -66,34 +64,56 @@ func main() {
 	//}
 	//fmt.Println("upsert QE identity tx submitted:", tx.Hash().Hex())
 
-	fmspc, err := extractFMSPC(report)
-	if err != nil {
-		fmt.Println("err in extracting fmspc: ", err)
-	} else {
-		fmt.Println("fmspc: ", fmspc)
-	}
+	//fmspc, err := extractFMSPC(report)
+	//if err != nil {
+	//	fmt.Println("err in extracting fmspc: ", err)
+	//} else {
+	//	fmt.Println("fmspc: ", fmspc)
+	//}
 
 	// 10. Submit Quote for On-Chain Remote Attestation
-	parsedReport, err := egoenclave.VerifyRemoteReport(report)
-	if err != nil {
-		fmt.Println("error: ", err)
-	} else {
-		fmt.Println("generated report")
-		fmt.Println("data (pub key): ", hexutil.Encode(parsedReport.Data))
-		fmt.Println("security version: ", parsedReport.SecurityVersion)
-		fmt.Println("unique id: ", hexutil.Encode(parsedReport.UniqueID))
-		fmt.Println("signer id: ", hexutil.Encode(parsedReport.SignerID))
-		fmt.Println("product id: ", hexutil.Encode(parsedReport.ProductID))
-		fmt.Println("tcb status: ", parsedReport.TCBStatus)
-		fmt.Println("tcb advisories: ", parsedReport.TCBAdvisories)
-		fmt.Println("tcb advisories err: ", parsedReport.TCBAdvisoriesErr)
-	}
+	//parsedReport, err := egoenclave.VerifyRemoteReport(report)
+	//if err != nil {
+	//	fmt.Println("error: ", err)
+	//} else {
+	//	fmt.Println("generated report")
+	//	fmt.Println("data (pub key): ", hexutil.Encode(parsedReport.Data))
+	//	fmt.Println("security version: ", parsedReport.SecurityVersion)
+	//	fmt.Println("unique id: ", hexutil.Encode(parsedReport.UniqueID))
+	//	fmt.Println("signer id: ", hexutil.Encode(parsedReport.SignerID))
+	//	fmt.Println("product id: ", hexutil.Encode(parsedReport.ProductID))
+	//	fmt.Println("tcb status: ", parsedReport.TCBStatus)
+	//	fmt.Println("tcb advisories: ", parsedReport.TCBAdvisories)
+	//	fmt.Println("tcb advisories err: ", parsedReport.TCBAdvisoriesErr)
+	//}
 
 	//txQuote, err := ethClient.VerifyAndAttestOnChain(report)
 	//if err != nil {
 	//	log.Fatalf("failed to submit quote: %v", err)
 	//}
 	//fmt.Println("quote verification tx submitted:", txQuote.Hash().Hex())
+
+	expected := make([]byte, 32)
+	for i := 0; i < 32; i++ {
+		expected[i] = byte(i)
+	}
+
+	var reportData [64]byte
+	copy(reportData[:32], expected)
+
+	log.Printf("Local reportData[0:32] = %s", hex.EncodeToString(reportData[:32]))
+	log.Printf("Local reportData[32:64] = %s", hex.EncodeToString(reportData[32:]))
+
+	quote, err := enclave.GetRemoteReport(reportData[:])
+	if err != nil {
+		log.Fatalf("failed to generate remote report: %v", err)
+	}
+
+	log.Printf("Quote length = %d", len(quote))
+	if len(quote) >= 432 {
+		reportDataFromQuote := quote[368:432]
+		log.Printf("Extracted report_data = %s", hex.EncodeToString(reportDataFromQuote))
+	}
 }
 
 func extractFMSPC(quote []byte) (string, error) {
